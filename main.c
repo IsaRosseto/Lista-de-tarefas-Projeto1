483209fcfb209f24cc4d732a29e281d54074a807
Isabella Rosseto <92994715+IsaRosseto@users.noreply.github.com>
2023-09-24 15:50:51 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "Biblioteca.h"
+
+int main() {
+    struct Tarefa tarefas[MAX_TAREFAS];
+    int numTarefas = 0;
+
+    lerTarefas(tarefas, &numTarefas);
+
+    int opcao;
+    while (1) {
+        printf("Escolha uma opcao:\n");
+        printf("1. Cadastrar tarefa\n");
+        printf("2. Listar tarefas\n");
+        printf("3. Deletar tarefa\n");
+        printf("4. Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarTarefa(tarefas, &numTarefas);
+                break;
+            case 2:
+                listarTarefas(tarefas, numTarefas);
+                break;
+            case 3:
+                if (numTarefas > 0) {
+                    int indice;
+                    printf("Informe o id da tarefa a ser deletada: ");
+                    scanf("%d", &indice);
+                    deletarTarefa(tarefas, &numTarefas, indice - 1);
+                } else {
+                    printf("Não ha tarefas cadastradas.\n");
+                }
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    }
+
+    return 0;
+}
